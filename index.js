9d534a1ea1fc712a4f6839ab9303a6bf893d0112
Brandon Nguyen <z5205609@student.unsw.edu.au>
2021-05-02 11:17:05 +1000

    Initialised basic structure of the game

---
+++ index.js
@@ -0,0 +1,26 @@
+// Makes thet table for stacker game
+const initTable = (tableElem, numCols, numRows) => {
+  // Makes a row with a specified number of columns
+  const initRow = (numCols) => {
+    const row = document.createElement("tr");
+    // Loop to make a col element and add cols to it
+    for (let index = 0; index < numCols; index++) {
+      const col = document.createElement("td");
+      col.classList.add("cell");
+      row.appendChild(col);
+    }
+    return row;
+  };
+
+  // Makes rows
+  for (let index = 0; index < numRows; index++) {
+    tableElem.appendChild(initRow(numCols));
+  }
+};
+const initGame = () => {
+  console.log("initialising");
+  const table = document.getElementById("game-table");
+  initTable(table, 7, 15);
+};
+
+window.addEventListener("DOMContentLoaded", initGame);
